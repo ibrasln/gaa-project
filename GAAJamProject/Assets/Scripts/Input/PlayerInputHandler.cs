e5ee50e24563544356cd59d621a1b131882cce5d
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2023-04-08 23:19:25 +0300

    Input Handler has been created

--- GAAJamProject/Assets/Scripts/Input/PlayerInputHandler.cs
+++ GAAJamProject/Assets/Scripts/Input/PlayerInputHandler.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Movement Inputs
+    public Vector2 MovementInput { get; private set; }
+    public float InputX { get; private set; }
+    public float InputY { get; private set; }
+    #endregion
+
+    #region Ability Inputs
+    public bool RollInput { get; private set; }
+    public bool MeleeAttackInput { get; private set; }
+    public bool RangeAttackInput { get; private set; }
+    #endregion
+
+    public void Move(InputAction.CallbackContext context)
     {
-        
+        MovementInput = context.ReadValue<Vector2>();
+        InputX = MovementInput.x;
+        InputY = MovementInput.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Roll(InputAction.CallbackContext context)
     {
-        
+        if (context.started) RollInput = true;
+        else if (context.canceled) RollInput = false;
+    }
+
+    public void MeleeAttack(InputAction.CallbackContext context)
+    {
+        if (context.started) MeleeAttackInput = true;
+        if (context.canceled) MeleeAttackInput = false;
+    }
+
+    public void RangeAttack(InputAction.CallbackContext context)
+    {
+        if (context.started) RangeAttackInput = true;
+        if (context.canceled) RangeAttackInput = false;
     }
 }
